6582e6cc054490f250c22eac642941d976588ef9
Arjun Bhatia <arbhatia@microsoft.com>
2023-10-11 17:41:27 -0700

    code refactoring and added prompts for user inputs

--- DeleteADOPipelineBuildLeases/DeleteBuildLeases.cs
+++ DeleteADOPipelineBuildLeases/DeleteBuildLeases.cs
@@ -1,21 +1,20 @@
 ﻿using Newtonsoft.Json;
 using System.Net.Http.Headers;
 using System.Text;
-using Microsoft.Extensions.Configuration;
 
 namespace DeleteADOPipelineBuildLeases
 {
     /// <summary>
-    /// This C# console application streamlines the deletion process of build pipelines in Azure DevOps. 
+    /// This C# console application automates the deletion of a build pipeline in Azure DevOps using the Azure DevOps REST API.
     /// It addresses the issue where build leases on each build within the pipeline prevent you from deleting the build pipeline. Removing all these build leases manually is a time-consuming task, and this code is designed to automate the process.
-    /// Once all build leases are removed from each build, you can delete the build pipeline as usual in Azure DevOps.
     /// 
-    /// Before running the application, please ensure that you configure the following variables with appropriate values:
-    /// - 'personalAccessToken': Your Azure DevOps Personal Access Token (PAT) for authentication. Go to user settings -> Personal Access Tokens in ADO and create a new token with appropriate permissions for modifiying pipelines. Copy the token and paste it below
-    /// - 'buildDefinitionId': The unique identifier of the specific build pipeline you want to manage.
-    /// - 'organization': The name of your Azure DevOps organization
-    /// - 'project': The name of your Azure DevOps project
+    /// Please make sure to enter the following values while running the application:
+    /// - 'Personal Access Token': Your Azure DevOps Personal Access Token (PAT) for authentication. Go to user settings -> Personal Access Tokens in ADO and create a new token with appropriate permissions for modifiying pipelines. Copy the token
+    /// - 'Build Definition Id': The unique identifier of the specific build pipeline you want to manage. You can find it in the url of the build pipeline in ADO
+    /// - 'organization': The name of your Azure DevOps organization. Example: MicrosoftIT
+    /// - 'project': The name of your Azure DevOps project. Example: OneITVSO
     /// </summary>
+    ///
 
     class DeleteBuildLeases
     {
@@ -23,30 +22,51 @@ namespace DeleteADOPipelineBuildLeases
 
         static async Task Main(string[] args)
         {
-            var configuration = LoadConfiguration();
-
-            client = InitializeHttpClient(configuration);
-
-            string buildDefinitionId = configuration["BuildDefinitionId"];
-            List<Build> builds = await GetAllBuilds(buildDefinitionId);
-
-            var deleteTasks = new List<Task>();
-            foreach (Build build in builds)
+            try
             {
-                if (build.RetainedByRelease)
-                {
-                    List<BuildLease> leases = await GetBuildLeases(build.Id);
+                var personalAccessToken = GetInput("Enter your ADO Personal Access Token: ").Trim();
+                var buildDefinitionId = GetInput("Enter the Build Definition ID (from the url in ado): ").Trim();
+                var organization = GetInput("Enter your Azure DevOps organization: ").Trim();
+                var project = GetInput("Enter your Azure DevOps project name: ").Trim();
+                var deletePipelineConfirmation = GetInput("Do you want to delete the pipeline (y/n)? If n is entered, it will only delete all build leases but will not delete the pipeline: ").Trim().ToLower();
 
-                    foreach (BuildLease lease in leases)
+                client = InitializeHttpClient(personalAccessToken, organization, project);
+
+                List<Build> builds = await GetAllBuilds(buildDefinitionId);
+
+                var deleteTasks = new List<Task>();
+
+                foreach (Build build in builds)
+                {
+                    if (build.RetainedByRelease)
                     {
-                        Task deleteTask = DeleteBuildLease(build.Id, lease.LeaseId);
-                        deleteTasks.Add(deleteTask);
+                        List<BuildLease> leases = await GetBuildLeases(build.Id);
+
+                        foreach (BuildLease lease in leases)
+                        {
+                            Task deleteTask = DeleteBuildLease(build.Id, lease.LeaseId);
+                            deleteTasks.Add(deleteTask);
+                        }
                     }
                 }
+
+                await Task.WhenAll(deleteTasks);
+                Console.WriteLine($"Successfully deleted leases for all builds within the pipeline: {buildDefinitionId} in ADO");
+
+                if (deletePipelineConfirmation == "y") {
+                    await DeletePipeline(buildDefinitionId);
+                    Console.WriteLine($"Successfully deleted the build pipeline with id: {buildDefinitionId} in ADO");
+                }
+
+                Console.WriteLine("Press Enter to exit...");
+                Console.ReadLine();
             }
-            await Task.WhenAll(deleteTasks);
-            Console.WriteLine("Successfully deleted all build leases. Now the pipelines can be deleted as usual in ADO");
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
         }
+
 
         static async Task<List<Build>> GetAllBuilds(string buildDefinitionId)
         {
@@ -111,20 +131,31 @@ namespace DeleteADOPipelineBuildLeases
             }
         }
 
-        static IConfiguration LoadConfiguration()
+        static async Task DeletePipeline(string buildDefinitionId)
         {
-            return new ConfigurationBuilder()
-                .SetBasePath(AppContext.BaseDirectory)
-                .AddJsonFile("appsettings.json")
-                .Build();
+            try
+            {
+                var response = await client.DeleteAsync($"_apis/build/definitions/{buildDefinitionId}?api-version=7.2-preview.7");
+                var responseBody = await response.Content.ReadAsStringAsync();
+                if (!response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine($"Failed to delete pipeline wit id: ${buildDefinitionId}. Status: ${response.StatusCode}. Response Content: {responseBody}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exception message: {ex.Message}");
+            }
         }
 
-        static HttpClient InitializeHttpClient(IConfiguration configuration)
+        static string GetInput(string prompt)
         {
-            string organization = configuration["Organization"];
-            string project = configuration["Project"];
-            string personalAccessToken = configuration["PersonalAccessToken"];
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
 
+        static HttpClient InitializeHttpClient(string personalAccessToken, string organization, string project)
+        {
             var httpClient = new HttpClient();
             httpClient.BaseAddress = new Uri($"https://dev.azure.com/{organization}/{project}/");
             httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.ASCII.GetBytes($":{personalAccessToken}")));
